b5f8db95c9a7f61526227a165edf039750e4b17e
Lars <leetruff@gmail.com>
2017-03-22 16:45:36 +0100

    Rectangle created + moveable left/right

--- core/src/com/ruffles/galaga/GameScreen.java
+++ core/src/com/ruffles/galaga/GameScreen.java
@@ -1,21 +1,73 @@
 package com.ruffles.galaga;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class GameScreen implements Screen {
 
+	PlayerShip playership;
+	
+	MyGdxGame game;
+	OrthographicCamera cam;
+	StretchViewport port;
+	SpriteBatch batch;
+	
 	public GameScreen(MyGdxGame myGdxGame, int i) {
-
+		this.game = myGdxGame;
 	}
 
 	@Override
 	public void show() {
 		System.out.println("GameScreen shown");
+		playership = new PlayerShip();
+		
+		cam = new OrthographicCamera();
+		port = new StretchViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_HEIGHT, cam);
+		cam.position.set(port.getWorldWidth() / 2, port.getWorldHeight() / 2, 0);
+		
+		batch = new SpriteBatch();
 	}
 
 	@Override
 	public void render(float delta) {
 
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		update(delta);
+		
+		
+		batch.setProjectionMatrix(cam.combined);
+		
+		batch.begin();
+		playership.getShaperenderer().begin();
+		playership.getShaperenderer().rect(playership.getPosX(), playership.getPosY(), playership.getBounds().width, playership.getBounds().height);
+		playership.getShaperenderer().end();
+		batch.end();
+	}
+
+	private void update(float delta) {
+
+		if(Gdx.input.isKeyPressed(Keys.D)){
+			playership.setPosX(playership.getPosX() + 7);
+			
+			if(playership.getPosX() > 490 - playership.getBounds().getWidth()){
+				playership.setPosX((int) (490 - playership.getBounds().getWidth()));
+			}
+		}
+		
+		if(Gdx.input.isKeyPressed(Keys.A)){
+			playership.setPosX(playership.getPosX() - 7);
+			
+			if(playership.getPosX() < 0){
+				playership.setPosX(0);
+			}
+		}
 	}
 
 	@Override
---
+++ core/src/com/ruffles/galaga/PlayerShip.java
@@ -0,0 +1,50 @@
+package com.ruffles.galaga;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Rectangle;
+
+public class PlayerShip {
+
+	Rectangle bounds;
+
+	int posX = 0;
+	int posY = 0;
+	
+	ShapeRenderer shaperenderer;
+	
+	public ShapeRenderer getShaperenderer() {
+		return shaperenderer;
+	}
+
+	public PlayerShip(){
+		bounds = new Rectangle(0, 0, 30, 30);
+		shaperenderer = new ShapeRenderer();
+		shaperenderer.setAutoShapeType(true);
+		shaperenderer.begin();
+		shaperenderer.set(ShapeType.Filled);
+		shaperenderer.setColor(Color.YELLOW);
+		shaperenderer.end();
+	}
+
+	public Rectangle getBounds() {
+		return bounds;
+	}
+	
+	public int getPosX() {
+		return posX;
+	}
+	
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	
+	public int getPosY() {
+		return posY;
+	}
+	
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+}
