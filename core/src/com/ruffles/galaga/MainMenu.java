ab433c7621649e25dc06a83512fddd7217aa0c95
Lars <leetruff@gmail.com>
2017-03-22 15:36:01 +0100

    super.render() and creation of MainMenue class

---
+++ core/src/com/ruffles/galaga/MainMenu.java
@@ -0,0 +1,46 @@
+package com.ruffles.galaga;
+
+import com.badlogic.gdx.Screen;
+
+public class MainMenu implements Screen{
+
+	public MainMenu(MyGdxGame myGdxGame, int i) {
+
+	}
+
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		System.out.println("test");
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
