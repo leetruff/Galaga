bef621bf4777f9dbef501ac069617855ba3c1bb6
Lars <lars.lissek@tu-dortmund.de>
2017-03-22 17:59:29 +0100

    Enemy and Swarm Classes

---
+++ core/src/com/ruffles/galaga/Enemy.java
@@ -0,0 +1,16 @@
+package com.ruffles.galaga;
+
+
+/*
+ * Abstract class, which models enemies in general
+ */
+public abstract class Enemy {
+
+	int hitpoints;
+	
+	
+	
+	public Enemy(){
+		
+	}
+}
---
+++ core/src/com/ruffles/galaga/Swarm.java
@@ -0,0 +1,8 @@
+package com.ruffles.galaga;
+
+/*
+ * Class, which models a swarm of enemys flying onto the playfield
+ */
+public class Swarm {
+
+}
